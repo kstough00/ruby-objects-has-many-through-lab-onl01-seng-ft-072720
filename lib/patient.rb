fdd3e448846f88951949df6cf0eb2b767a2949fc
Kyle Stough <kstough00@gmail.com>
2020-08-12 20:28:49 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,12 +9,12 @@ class Patient
     @@all << self
   end
   
-  def self.all
-    @@all
-  end
-  
   def new_appointment(date, doctor)
     Appointment.new(date, self, doctor)
+  end
+  
+  def self.all
+    @@all
   end
   
   def appointments
