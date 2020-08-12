0615403ef1d9f34fd298a0efd4bff8c2e4257184
Kyle Stough <kstough00@gmail.com>
2020-08-12 20:14:33 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,8 +2,33 @@ class Doctor
   
   attr_accessor :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
   end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
+  def patients
+    Appointments.all.select {|appointment| appointment.doctor == self}
+    appointment.map {|appointment| appointment.doctor}
+  end
+  
+  # def artists
+  #   Song.all.select {|song| song.artist == self}
+  #   songs.map {|song| song.artist}
+  # end
   
 end
--- lib/patient.rb
+++ lib/patient.rb
@@ -2,8 +2,24 @@ class Patient
   
   attr_accessor :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
   end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Patient.all.select {|patient| [patient].doctor == self}
+  end
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+  
   
 end
